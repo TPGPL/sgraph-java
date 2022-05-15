bb603153545933d8c269089b05d905317bb9b3c6
TPGPL <mateuszkwiatkowski2002@gmail.com>
2022-05-15 23:13:55 +0200

    Implement the node connection class

--- src/main/java/Connection.java
+++ src/main/java/Connection.java
@@ -1,4 +1,33 @@
 public class Connection {
-    private Node node;
-    private double weight;
+    private final Node node;
+    private final double weight;
+
+    public Connection(Node node, double weight)
+    {
+        this.node = node;
+        this.weight = weight;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public Node getNode()
+    {
+        return node;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Connection co)
+            return co.node.getIndex() == this.node.getIndex(); // comparing only through node index to simplify implementation
+
+        return false;
+    }
+
+    @Override
+    public String toString()
+    {
+        return node.getIndex() + ":" + weight;
+    }
 }
