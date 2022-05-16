4bbe08986f9d3b968344d6a7abf4cfdad6e2957f
TPGPL <mateuszkwiatkowski2002@gmail.com>
2022-05-16 13:44:17 +0200

    Implement basic Graph and Node class structure

--- src/main/java/Graph.java
+++ src/main/java/Graph.java
@@ -1,9 +1,150 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Graph {
-    private int columnCount;
-    private int rowCount;
+    private final int columnCount;
+    private final int rowCount;
     private int subgraphCount;
-    private ArrayList<Node> nodes;
+    private final ArrayList<Node> nodes;
     private Range edgeValueRange;
+
+    public Graph(int columnCount, int rowCount, int subgraphCount)
+    {
+        if (columnCount <= 0)
+            throw new IllegalArgumentException("Graph: The number of columns must be positive.");
+
+        if (rowCount <= 0)
+            throw new IllegalArgumentException("Graph: The number of rows must be positive.");
+
+        if (subgraphCount <= 0)
+            throw new IllegalArgumentException("Graph: The number of subgraphs must be positive.");
+
+        this.columnCount = columnCount;
+        this.rowCount = rowCount;
+        this.subgraphCount = subgraphCount;
+
+        nodes = new ArrayList<>();
+
+        for (int i = 0; i < columnCount * rowCount; i++) {
+            nodes.add(new Node(i));
+        }
+    }
+
+    public Graph(int columnCount, int rowCount, int subgraphCount, double min, double max)
+    {
+        this(columnCount, rowCount, subgraphCount);
+
+        edgeValueRange = new Range(min, max);
+    }
+
+    public int getColumnCount()
+    {
+        return columnCount;
+    }
+
+    public int getRowCount()
+    {
+        return rowCount;
+    }
+
+    public int getSubgraphCount() {
+        return subgraphCount;
+    }
+
+    public Range getEdgeValueRange()
+    {
+        return edgeValueRange;
+    }
+
+    public Node getNode(int index) throws IllegalArgumentException
+    {
+        if (index < 0 || index >= columnCount * rowCount)
+            throw new IllegalArgumentException(String.format("Graph: Cannot get a node of index %d in a %dx%d graph.", index, rowCount, columnCount));
+
+        return nodes.get(index);
+    }
+
+    public void addConnection(Node node1, Node node2, double edge) throws IllegalArgumentException
+    {
+        if (!canNodesAdhere(node1, node2))
+            throw new IllegalArgumentException(String.format("Graph: Nodes %d and %d cannot adhere in a %dx%d graph.", node1.getIndex(), node2.getIndex(), rowCount, columnCount));
+
+        if (edge <= 0)
+            throw new IllegalArgumentException("Graph: Edge value must be positive.");
+
+        if (node1.hasConnection(node2)) // connection between node1 and node2 exists
+        {
+            double definedEdge = node1.getEdgeOnConnection(node2);
+
+            if (definedEdge == edge) // the edge values are equal -> likely has been added the second time through IO
+                return;
+            else
+                throw new IllegalArgumentException(String.format("Graph: Connection between nodes %d and %d has already been defined with an edge value of %g", node1.getIndex(), node2.getIndex(), definedEdge));
+        }
+
+        node1.addConnection(node2, edge);
+        node2.addConnection(node1, edge);
+    }
+
+    public void removeConnection(Node node1, Node node2)
+    {
+        node1.removeConnection(node2);
+        node2.removeConnection(node1);
+    }
+
+    private boolean canNodesAdhere(Node node1, Node node2)
+    {
+        int row1 = (node1.getIndex() - node1.getIndex() % columnCount) / columnCount + 1;
+        int row2 = (node2.getIndex() - node2.getIndex() % columnCount) / columnCount + 1;
+        int col1 = node1.getIndex() % columnCount + 1;
+        int col2 = node2.getIndex() % columnCount + 1;
+
+        return Math.abs(row1 - row2) == 1 || Math.abs(col1 - col2) == 1;
+    }
+
+    public void setEdgeValueRange(double min, double max)
+    {
+        edgeValueRange = new Range(min, max);
+    }
+
+    public void generate()
+    {
+        Random r = new Random();
+
+        for (int i = 0; i < rowCount * columnCount; i++) {
+            if (i % columnCount + 1 != columnCount) // if node is not in the last column
+                addConnection(nodes.get(i), nodes.get(i + 1), r.nextDouble(edgeValueRange.getMin(), edgeValueRange.getMax()));
+
+            if ((i - i % columnCount) / columnCount + 1 != rowCount) // if node is not in the last row
+                addConnection(nodes.get(i), nodes.get(i + columnCount), r.nextDouble(edgeValueRange.getMin(), edgeValueRange.getMax()));
+        }
+
+        if (subgraphCount != 1)
+            divide(); // TODO
+    }
+
+    public void readFromFile(String path)
+    {
+        return;
+    }
+
+    public void readToFile(String path) throws IOException
+    {
+        BufferedWriter writer = new BufferedWriter(new FileWriter(path));
+        writer.write(String.format("%d %d\n", rowCount, columnCount));
+
+        for (Node n : nodes) {
+            writer.write(n.toString() + "\n");
+        }
+
+        writer.close();
+    }
+
+    public void divide()
+    {
+        return;
+    }
 }
--- src/main/java/Node.java
+++ src/main/java/Node.java
@@ -1,6 +1,66 @@
 import java.util.ArrayList;
 
 public class Node {
-    private int index;
-    private ArrayList<Connection> connections;
+    private final int index;
+    private final ArrayList<Connection> connections;
+
+    public Node(int index)
+    {
+        this.index = index;
+        connections = new ArrayList<>();
+    }
+
+    public int getIndex()
+    {
+        return index;
+    }
+
+    public void addConnection(Node node, double edge)
+    {
+        connections.add(new Connection(node, edge));
+    }
+
+    public void removeConnection(Node node)
+    {
+        connections.remove(new Connection(node, 0));
+    }
+
+    public boolean hasConnection(Node node)
+    {
+        return connections.contains(new Connection(node, 0));
+    }
+
+    public int getAdherentNumber()
+    {
+        return connections.size();
+    }
+
+    public double getEdgeOnConnection(Node node)
+    {
+        int index = connections.indexOf(new Connection(node, 0));
+
+        if (index == -1)
+            return 0;
+
+        return connections.get(index).getWeight();
+    }
+
+    @Override
+    public String toString()
+    {
+        String text = "\t\t";
+
+        for (Connection c : connections)
+            text = text.concat(c.toString() + " ");
+
+        return text;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Node n)
+            return n.getIndex() == this.getIndex();
+
+        return false;
+    }
 }
