4c6131e58c74096049e5489f94fb2bfc9d731892
TPGPL <TPGPL@users.noreply.github.com>
2022-06-01 23:35:28 +0200

    Add javadoc for BreadthFirstSearch class

--- src/main/java/org/sgraph/BreadthFirstSearch.java
+++ src/main/java/org/sgraph/BreadthFirstSearch.java
@@ -3,11 +3,27 @@ package org.sgraph;
 import java.util.ArrayList;
 import java.util.LinkedList;
 
+/**
+ * Klasa odpowiadająca za działanie algorytmu przeszukiwania wszerz (BFS) dla grafu.
+ */
 public class BreadthFirstSearch {
+    /**
+     * Tablica przechowująca informacje o tym, czy wierzchołek o danym indeksie został odwiedzony przez BFS.
+     */
     private final boolean[] visitedNodes;
+    /**
+     * Tablica przechowująca wierzchołki połączone z wierzchołkiem, od którego BFS rozpoczął działanie.
+     */
     private final ArrayList<Node> connectedNodes;
+    /**
+     * Kolejka FIFO przechowująca wierzchołki, które wymagają odwiedzenia.
+     */
     private final LinkedList<Node> queue;
 
+    /**
+     * Konstruktor klasy
+     * @param nodeNumber liczba wierzchołków w grafie
+     */
     public BreadthFirstSearch(int nodeNumber) {
         if (nodeNumber <= 0)
             throw new IllegalArgumentException("BreadthFirstSearch: The number of nodes in graph must be positive.");
@@ -17,6 +33,11 @@ public class BreadthFirstSearch {
         connectedNodes = new ArrayList<>();
     }
 
+    /**
+     * Rozpoczyna działanie algorytmu BFS i przechodzi po wszystkich połączonych wierzchołkach wszerz.
+     * Dołącza odwiedzone wierzchołki do tablicy connectedNodes i oznacza je jako odwiedzone.
+     * @param startNode wierzchołek od którego rozpoczyna się działanie algorytmu
+     */
     public void run(Node startNode)
     {
         Node parsedNode;
@@ -37,6 +58,10 @@ public class BreadthFirstSearch {
         }
     }
 
+    /**
+     * Sprawdza, czy w grafie jest jakiś nieodwiedzony wierzchołek.
+     * @return true, jeżeli jakiś wierzchołek nie został odwiedzony, w przeciwnym razie false
+     */
     public boolean hasNotVisitedNode()
     {
         for (boolean visitedNode : visitedNodes) {
@@ -47,6 +72,11 @@ public class BreadthFirstSearch {
         return false;
     }
 
+    /**
+     * Zwraca indeks nieodwiedzonego wierzchołka o najmniejszym indeksie.
+     * Jeżeli nie ma takiego wierzchołka, zwraca -1.
+     * @return indeks nieodwiedzonego wierzchołka
+     */
     public int getNotVisitedNode()
     {
         for (int i = 0; i < visitedNodes.length; i++)
@@ -56,11 +86,10 @@ public class BreadthFirstSearch {
         return -1;
     }
 
-    public boolean wasNodeVisited(Node node)
-    {
-        return visitedNodes[node.getIndex()];
-    }
-
+    /**
+     * Zwraca tablicę wierzchołków połączonych z wierzchołkiem, od którego algorytm BFS rozpoczął działanie.
+     * @return tablica wierzchołków połączonych z wierzchołkiem początkowym
+     */
     public ArrayList<Node> getConnectedNodes() {
         return connectedNodes;
     }
