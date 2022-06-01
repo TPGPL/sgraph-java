e341d5bd40355e682e1a01cad4673299656c13d6
TPGPL <TPGPL@users.noreply.github.com>
2022-06-01 23:46:40 +0200

    Add javadoc for Connection class

--- src/main/java/org/sgraph/Connection.java
+++ src/main/java/org/sgraph/Connection.java
@@ -1,9 +1,23 @@
 package org.sgraph;
 
+/**
+ * Klasa przechowująca informacje o ważonym połączeniu z określonym wierzchołkiem w grafie.
+ */
 public class Connection {
+    /**
+     * Wierzchołek, z którym istnieje połączenie.
+     */
     private final Node node;
+    /**
+     * Wartość wagi na krawędzi połączenia z wierzchołkiem.
+     */
     private final double weight;
 
+    /**
+     * Konstruktor klasy
+     * @param node wierzchołek, z którym istnieje połączenie
+     * @param weight wartość wagi na krawędzi połączenia
+     */
     public Connection(Node node, double weight)
     {
         if (weight <= 0)
@@ -13,15 +27,29 @@ public class Connection {
         this.weight = weight;
     }
 
+    /**
+     * Zwraca wartość wagi na krawędzi połączenia.
+     * @return wartość wagi na krawędzi połączenia
+     */
     public double getWeight() {
         return weight;
     }
 
+    /**
+     * Zwraca wierzchołek, z którym istnieje połączenie.
+     * @return wierzchołek, z którym istnieje połączenie
+     */
     public Node getNode()
     {
         return node;
     }
 
+    /**
+     * Sprawdza, czy przekazany obiekt jest równy obiektowi klasy.
+     * Sprawdzenie odbywa się poprzez porównanie typów obiektów, a jeżeli są równe, to poprzez indeksy wierzchołków, z którymi istnieje połączenie.
+     * @param obj porównywany obiekt
+     * @return true, jeżeli obiekty są równe; w przeciwnym razie - false
+     */
     @Override
     public boolean equals(Object obj) {
         if (obj instanceof Connection co)
@@ -30,6 +58,10 @@ public class Connection {
         return false;
     }
 
+    /**
+     * Zwraca napis postaci "[indeks wierzchołka]:[waga]" reprezentujący aktualny stan obiektu.
+     * @return napis reprezentujący połączenie
+     */
     @Override
     public String toString()
     {
